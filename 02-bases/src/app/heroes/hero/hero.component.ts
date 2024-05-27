fef8eef4a133eb5c847b3a25bc5dc91e943b5ab4
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-27 07:23:00 -0500

    Cambiar nombre y edad

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -17,4 +17,12 @@ export class HeroComponent {
     return `${this.name} - ${this.age}`
   }
 
+  changeHero() : void {
+    this.name = 'Spiderman'  
+  }
+
+  changeAge() : void {
+    this.age = 36;
+  }
+
 }
