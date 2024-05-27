09b84d912787f0d8e5a3e5d6b8cd6b274958f4dd
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-27 07:17:06 -0500

    One way data binding - enlazado en una sola vía

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -9,5 +9,12 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
-  
+  get capitilizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`
+  }
+
 }
