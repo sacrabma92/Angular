08cd36b2489f3432201ebbe9a386e8987cc7bd52
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-31 09:10:49 -0500

    DBZ en Componentes

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { DbzComponent } from './pages/main-page.component';
+import { ListdbzComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    DbzComponent
+    DbzComponent,
+    ListdbzComponent,
+    AddCharacterComponent
   ],
   exports: [
     DbzComponent
